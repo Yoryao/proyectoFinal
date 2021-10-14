38b4298312a382997c33a22ba7695f10bca96590
yoryao <jorgejoaquinrivera@gmail.com>
2021-10-14 00:03:31 -0300

    start coding on doctor

--- app.js
+++ app.js
@@ -7,16 +7,34 @@ class Paciente {
         this.dni = dni,
         this.email = email,
         this.doctor = doctor,
-        this.cobertura = cobertura
+        this.cobertura = cobertura,
+        this.ingresado = false,
+        this.atendido = false,
+        this.retirado = true
     }
 
     ingresar() {
-        console.log("El paciente fue ingresado.")
-    }
+        if (this.ingresado != true) {
+                this.ingresado = true;
+                this.retirado = false;
+                console.log("El paciente fue ingresado.")
+            } else {
+                console.log("El paciente ya se encuentra ingresado.")
+            }
+    };
 
     egresar() {
-        console.log("El paciente se ha retirado.")
-    }
+        if (this.retirado != true) {
+            this.retirado = true;
+            this.ingresado = false;
+            console.log("El paciente se ha retirado.")
+        } else {
+            console.log("El paciente no esta en el centro.")
+        }
+    };
+    
+
+
 
     reservarTurno(){
 //por parametro se ingresa: doctor, fecha, hora
@@ -31,6 +49,11 @@ class Paciente {
 
 };
 
+const paciente1 = new Paciente ("Hugo", "Reyes", 14256528, "hugo@gmail.com", "Cormillot", "Osde");
+
+console.log(paciente1);
+paciente1.ingresar();
+paciente1.egresar();
 
 
 //MOSTRAR LOS PACIENTES EN FILA POR MEDICO
@@ -39,10 +62,30 @@ class Paciente {
 
 
 //DEFINIR LA CLASE PARA HABILITAR UN MEDICO AL CENTRO MEDICO
+class Doctor {
+    constructor ( apellido, dni, email, doctor, cobertura) {
+        this.apellido = apellido,
+        this.dni = dni,
+        this.cobertura = cobertura,
+        this.ingresado = false,
+        this.atendiendo = false,
+        this.retirado = true
+    }
+
+    atender(paciente1) {
+
+    }
+
+
+
+
+
+
+
+
 
 //MOSTRAR LOS PACIENTES PENDIENTES DE ATENCIÓN
 //MOSTRAR TIEMPO APROXIMADO PARA TERMINAR LAS CONSULTAS
-
 
 
 
@@ -53,7 +96,4 @@ class Paciente {
 -PERSONAL ADMINISTRATIVO.
 -MANEJO DE CAJA.
 -AGENDAR TURNO.
--VALIDAR OBRAS SOCIALES PERMITIDAS
-
-
-
+-VALIDAR OBRAS SOCIALES PERMITIDAS*/
