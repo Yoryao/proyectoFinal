5bdd75ff8561a78b256ccc1a8e30ce1c1256297f
yoryao <jorgejoaquinrivera@gmail.com>
2021-11-08 23:47:00 -0300

    jquery on the login ready

--- login.js
+++ login.js
@@ -2,8 +2,8 @@
 class Usuario {
   constructor(nombre, password) {
     (this.nombre = nombre), (this.password = password);
-  };
-};
+  }
+}
 
 //ARRAY VACIO PARA GUARDAR LOS USUARIOS Y ENVIAR AL STORAGE
 let usuarios = [];
@@ -12,79 +12,64 @@ let usuarios = [];
 //IMPEDIR CREACION DUPLICADA DE NOMBRE DE USUARIO
 
 //FUNCION REGISTRO DE USUARIOS
-function registro(e) {
+$("#btnRegistro").click(function (e) {
   e.preventDefault();
 
   // CAPTURO DATOS DEL USUARIO.
-  const nombreRegistro = document.getElementById("nombreRegistro").value;
-  const passwordRegistro = document.getElementById("passwordRegistro").value;
+  const nombreRegistro = $("#nombreRegistro").val();
+  const passwordRegistro = $("#passwordRegistro").val();
+
   let nombreExistente = usuarios.find((user) => user.nombre === nombreRegistro);
+
   if (!nombreExistente) {
+    //CREO EL OBJETO USUARIO Y LO ENVIO AL STORAGE CONVERTIDO EN JSON
+    const usuario = new Usuario(nombreRegistro, passwordRegistro);
+    usuarios.push(usuario);
 
-  //CREO EL OBJETO USUARIO Y LO ENVIO AL STORAGE CONVERTIDO EN JSON
-  const usuario = new Usuario(nombreRegistro, passwordRegistro);
-  usuarios.push(usuario);
+    //CON CADA CREACION DE USUARIO ENVIO LOS DATOS AL STORAGE.
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
 
-  //CON CADA CREACION DE USUARIO ENVIO LOS DATOS AL STORAGE.
-  localStorage.setItem("usuarios", JSON.stringify(usuarios));
-
-  mostrarDisplay("ya puede realizar el Login");
-
+    mostrarDisplay("ya puede realizar el Login");
   } else {
     mostrarDisplay("Ese nombre ya esta registrado.");
   }
-};
+});
 
 //FUNCION LOGIN DE USUARIO
-function login(e) {
-  e.preventDefault();  
+$("#logButton").click(function (e) {
+  e.preventDefault();
 
-  //EXTRAIGO LOS USUARIOS DEL STORAGE Y LO PARSEO. 
+  //EXTRAIGO LOS USUARIOS DEL STORAGE Y LO PARSEO.
   let usuariosRegistrados = JSON.parse(localStorage.getItem("usuarios"));
   //CAPTURO LOS DATOS INGRESADOS EN EL LOGIN
-  let nombreLogin = document.getElementById("nombrelogin").value;
-  let passwordLogin = document.getElementById("passwordLogin").value;
+  let nombreLogin = $("#nombreLogin").val();
+  let passwordLogin = $("#passwordLogin").val();
   //RECORRE LOS OBJETOS-USUARIOS CON EL PARAMETRO NOMBRE.
-  let usuarioRegistrado = usuariosRegistrados.find((user) => user.nombre === nombreLogin);
-  
-   //VERIFICO EQUIVALENCIA ENTRE PASSWORD INGRESA Y PASSWORD REGISTRADO.
-  if ((usuarioRegistrado) && (passwordLogin === `${usuarioRegistrado.password}`)){
+  let usuarioRegistrado = usuariosRegistrados.find(
+    (user) => user.nombre === nombreLogin
+  );
+
+  //VERIFICO EQUIVALENCIA ENTRE PASSWORD INGRESA Y PASSWORD REGISTRADO.
+  if (usuarioRegistrado && passwordLogin === `${usuarioRegistrado.password}`) {
     //passwords iguales: se habilita el ingreso.
-    
-    mostrarDisplay("ya puede ingresar al sistema.");
-    
+     mostrarDisplay("ya puede ingresar al sistema.");
+
     //GUARDO EL NOMBRE DEL USUARIO PARA LEVANTARLO DESDE EL INDEX.
     localStorage.setItem("usuario", JSON.stringify(nombreLogin));
 
-    const boton = document.getElementById("botonIngreso");
-    boton.innerHTML = `<button type="button" id="ingreso1" value="Ingresar al sistema"><a href="index.html" target="_blank">INGRESO AL SISTEMA</a></button>`;
-    document.body.append(boton);
-    } else {
-     
-      mostrarDisplay("Los datos de ingreso no coinciden.");
+    $("#botonIngreso").append(`<button type="button"
+                                id="ingreso1" value="Ingresar al sistema">
+                                <a href="index.html" target="_blank">INGRESO AL SISTEMA</a>
+                                </button>`);
+  } else {
+    mostrarDisplay("Los datos de ingreso no coinciden.");
+  }
+});
 
-    }
-};
-
-//EVENTOS DEL USUARIO
-
-let regButton = document.getElementById("btnRegistro");
-regButton.addEventListener("click", registro);
-
-let logButton = document.getElementById("logButton");
-logButton.addEventListener("click", login);
-
-// //FUNCIONES
-// function mostrarDisplay(msg) {
-//   let display = document.getElementById("encabezado");
-//   display.innerText = msg ;
-// };
-
-
+//FUNCIONES
 function mostrarDisplay(msg) {
-  $("#encabezado").text(msg)
-};
+  $("#encabezado").text(msg);
+}
 
 //VERIFICACIONES
 console.log(usuarios);
-
